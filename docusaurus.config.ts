f5030525da804c8ad7a5f7831c914e04a54cdb86
Moeenahamd <moeenranjha01@gmail.com>
2025-05-21 06:04:56 +0500

    navbar styling

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,64 +104,8 @@ const config: Config = {
          src: 'img/Logo.svg',
        },
        items: [ 
-        {to: '/swagger', label: 'API Playground', position: 'left'},
+        {to: '/swagger', label: 'API Playground', position: 'right'},
          
-        {to: 'https://www.omise.co/en/contact-us', label: 'Contact Us', position: 'left'},
-         
-        {to: 'https://dashboard.omise.co/v2', label: 'Sign In', position: 'left'},
-        // {
-        //   label: 'Products',
-        //   position: 'left',
-        //   type: 'dropdown',
-        //   items: [
-        //     { label: 'Product A', to: '/products/product-a' },
-        //     { label: 'Product B', to: '/products/product-b' },
-        //   ],
-        // },
-        // {
-        //   label: 'Company',
-        //   position: 'left',
-        //   type: 'dropdown',
-        //   items: [
-        //     { label: 'About Us', to: '/company/about' },
-        //     { label: 'Newsroom', to: '/company/newsroom' },
-        //     { label: 'Careers', to: '/company/careers' },
-        //   ],
-        // },
-        // {
-        //   label: 'Resources',
-        //   position: 'left',
-        //   type: 'dropdown',
-        //   items: [
-        //     { label: 'Customer Stories', to: '/customer-stories' },
-        //     { label: 'Blog', to: '/blog' },
-        //     { label: 'Pricing', to: '/resources/Pricing' },
-        //   ],
-        // },
-        //  {to: '/Documentation', label: 'Documentation', position: 'left'},
-         
-        // //  {to: '/Help-center', label: 'Help Center', position: 'left'},
-        // {
-        //   label: 'Help Center',
-        //   position: 'left',
-        //   type: 'dropdown',
-        //   items: [
-        //     { label: 'Get Started', to: '/help-center/get-started' },
-        //     { label: 'Contact us', to: '/help-center/contact' },
-        //     { label: 'FAQ', to: '/help-center/faq' },
-        //   ],
-        // },
-      
-        //  {to: '/blog', label: 'Blog', position: 'left'},
-        //  {
-        //    type: 'docsVersionDropdown',  
-        //    position: 'left',
-        //  },
-        //  {
-        //    href: 'https://github.com/facebook/docusaurus',
-        //    label: 'GitHub',
-        //    position: 'left',
-        //  },
         {
           type: 'docsVersionDropdown',  
           position: 'right',
@@ -171,17 +115,10 @@ const config: Config = {
           type: 'localeDropdown',
           position: 'right',
         },
-        // {
-        //   to: 'https://sso-idp.omise.co/realms/engagement/protocol/openid-connect/auth?client_id=dashboard&redirect_uri=https%3A%2F%2Fdashboard.omise.co%2Fv2&state=21e18e7e-b866-42ec-9e7c-517bd26728c0&response_mode=fragment&response_type=code&scope=openid&nonce=45e8c317-9207-4058-86cf-5786565a8e4e&ui_locales=en&code_challenge=Isb7eafaeQVGAcI0RKb-Xvlufr8uyzvkMIUEhx0yCCo&code_challenge_method=S256&_gl=1*1madhym*_gcl_au*OTQ3NDQ3NzkxLjE3NDU5MDI4NTk.',
-        //    label: 'Contact Us',
-        //    position: 'right',
-        //  },
-        // {
-        //  to: 'https://sso-idp.omise.co/realms/engagement/protocol/openid-connect/auth?client_id=dashboard&redirect_uri=https%3A%2F%2Fdashboard.omise.co%2Fv2&state=21e18e7e-b866-42ec-9e7c-517bd26728c0&response_mode=fragment&response_type=code&scope=openid&nonce=45e8c317-9207-4058-86cf-5786565a8e4e&ui_locales=en&code_challenge=Isb7eafaeQVGAcI0RKb-Xvlufr8uyzvkMIUEhx0yCCo&code_challenge_method=S256&_gl=1*1madhym*_gcl_au*OTQ3NDQ3NzkxLjE3NDU5MDI4NTk.',
-        //   label: 'Sign In',
-        //   position: 'right',
-        //   className: 'hide-on-mobile',
-        // }
+        
+        {to: 'https://www.omise.co/en/contact-us', label: 'Contact Us', position: 'right'},
+         
+        {to: 'https://dashboard.omise.co/v2', label: 'Sign In', position: 'right'},
        ],
      },
      algolia: {
